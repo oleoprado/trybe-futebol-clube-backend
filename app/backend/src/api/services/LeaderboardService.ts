7ffd3eb2b43496c4851292775b418ef29acc5108
Leonardo Prado <leonardo.rprado@gmail.com>
2023-03-03 09:43:05 +0000

    refactor: convertido para number o resultado da query teamsPerformace

--- app/backend/src/api/services/LeaderboardService.ts
+++ app/backend/src/api/services/LeaderboardService.ts
@@ -9,16 +9,16 @@ export default class LeaderboardService implements IServiceLeaderboard {
   async teamsPerformace(): Promise<Match[]> {
     return this.model.findAll({
       attributes: [
-        [fn('max', col('homeTeam.team_name')), 'name'],
+        [literal('homeTeam.team_name'), 'name'],
         [literal(`
-          (SUM(home_team_goals > away_team_goals) * 3) +
-          SUM(home_team_goals = away_team_goals)`), 'totalPoints'],
+          CAST((SUM(home_team_goals > away_team_goals) * 3) +
+          SUM(home_team_goals = away_team_goals) AS UNSIGNED)`), 'totalPoints'],
         [fn('COUNT', col('home_team_id')), 'totalGames'],
-        [fn('SUM', literal('home_team_goals > away_team_goals')), 'totalVictories'],
-        [fn('SUM', literal('home_team_goals = away_team_goals')), 'totalDraws'],
-        [fn('SUM', literal('away_team_goals > home_team_goals')), 'totalLosses'],
-        [fn('SUM', col('home_team_goals')), 'goalsFavor'],
-        [fn('SUM', col('away_team_goals')), 'goalsOwn'],
+        [literal('CAST(SUM(home_team_goals > away_team_goals) AS UNSIGNED)'), 'totalVictories'],
+        [literal('CAST(SUM(home_team_goals = away_team_goals) AS UNSIGNED)'), 'totalDraws'],
+        [literal('CAST(SUM(away_team_goals > home_team_goals) AS UNSIGNED)'), 'totalLosses'],
+        [literal('CAST(SUM(home_team_goals) AS UNSIGNED)'), 'goalsFavor'],
+        [literal('CAST(SUM(away_team_goals) AS UNSIGNED)'), 'goalsOwn'],
         // [literal('SUM(home_team_goals) - SUM(away_team_goals)'), 'goalsBalance'],
         // [literal(`
         //   ((SUM(home_team_goals > away_team_goals) * 3) +
